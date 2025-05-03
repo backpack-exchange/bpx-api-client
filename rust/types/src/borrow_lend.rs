e1ff091f070fe4ec955848ce680fa9e791617f7c
onkel <git@onkel.dev>
2025-05-03 13:33:13 +0200

    feat: remove json data

--- rust/types/src/borrow_lend.rs
+++ rust/types/src/borrow_lend.rs
@@ -3,28 +3,6 @@ use serde::{Deserialize, Serialize};
 
 use crate::margin::MarginFunction;
 
-// {
-//   "cumulativeInterest": "0.0001343044",
-//   "id": "1532703774",
-//   "imf": "0.1",
-//   "imfFunction": {
-//     "base": "0.1",
-//     "factor": "0.00036",
-//     "type": "sqrt"
-//   },
-//   "markPrice": "147.18280534",
-//   "mmf": "0.05",
-//   "mmfFunction": {
-//     "base": "0.05",
-//     "factor": "0.000216",
-//     "type": "sqrt"
-//   },
-//   "netExposureNotional": "0.022716253049297736",
-//   "netExposureQuantity": "0.0001543404",
-//   "netQuantity": "0.0001543404",
-//   "symbol": "SOL"
-// }
-
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct BorrowLendPosition {
