f1b523754c4adb05cd5e2918e3bf6c630ccc797d
Karun Matharu <karun.matharu@gmail.com>
2026-02-10 13:22:57 +0000

    fmt

--- client/src/routes/vault.rs
+++ client/src/routes/vault.rs
@@ -9,10 +9,7 @@ pub const API_VAULT_PENDING_REDEEMS: &str = "/api/v1/vault/redeems/pending";
 
 impl BpxClient {
     /// Fetches pending redeem requests for a vault.
-    pub async fn get_vault_pending_redeems(
-        &self,
-        vault_id: u32,
-    ) -> Result<Vec<VaultRedeem>> {
+    pub async fn get_vault_pending_redeems(&self, vault_id: u32) -> Result<Vec<VaultRedeem>> {
         let mut url = self.base_url.join(API_VAULT_PENDING_REDEEMS)?;
         url.query_pairs_mut()
             .append_pair("vaultId", &vault_id.to_string());
